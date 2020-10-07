29bc731cd925eb7fc667adc6119c55bc5c973dfd
barthflo <flrnt.barth@gmail.com>
2020-10-07 09:54:29 +0200

    working on submit form JS

--- JS/contact.js
+++ JS/contact.js
@@ -1,12 +1,17 @@
-const sendButton = document.querySelector('.button-container button');
+const form = document.querySelector('.form');
 
-sendButton.addEventListener('click', event => {
+form.addEventListener('submit', function(event){
     event.preventDefault();
     const car = document.getElementById('button-contact');
     const message = document.querySelector('form span');
     const button = document.querySelector('form button');
 
-    
+    message.classList.remove('hidden');
+
+    if(document.querySelector('textarea').value != ""){
+        document.querySelector('textarea').value = "";
+    }
+
     button.classList.toggle('send');
     car.classList.toggle('car-moving');
     message.classList.toggle('hidden');
@@ -14,8 +19,9 @@ sendButton.addEventListener('click', event => {
     car.addEventListener('animationend', () => {
         car.classList.remove('car-moving');
         button.classList.remove('send');
-    })
-      
+    }) 
+    document.querySelectorAll('.form input').forEach(input => input.value = "");
+    document.querySelector("text").value = "";  
 });
 
 // const validateForm = () =>
